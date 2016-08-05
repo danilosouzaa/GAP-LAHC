aec4663d2cd4573fab0da377d74a2a9f2f577da2
danilosouzaa <danilo.gdc@gmail.com>
2016-08-05 20:26:36 -0300

    allocation instance in gpu

---
+++ gap-lahc-demo/Solution.h
@@ -0,0 +1,17 @@
+#ifndef SOLUTION
+#define SOLUTION
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "Instance.h"
+
+#include "gpulib/types.h"
+
+
+EXTERN_C_BEGIN
+
+EXTERN_C_END
+
+#endif
---
+++ gap-lahc-demo/gInstance.cuh
@@ -0,0 +1,17 @@
+#ifndef INSTANCE_CUH_
+#define INSTANCE_CUH_
+
+#include "gpulib/gpu.cuh"
+
+extern "C" {
+
+#include "Instance.h"
+
+}
+
+// INCLUDE PROTOTYPE OF CUDA functions __device__ and __global__
+
+
+
+
+#endif /* INSTANCE_CUH_ */
