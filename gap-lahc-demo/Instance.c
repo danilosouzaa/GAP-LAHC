e2af0eafc9f4a427e8ee5705eb39fb300e80fc74
danilosouzaa <danilo.gdc@gmail.com>
2016-08-21 20:46:31 -0300

    ejection chains

--- gap-lahc-demo/Instance.c
+++ gap-lahc-demo/Instance.c
@@ -44,8 +44,9 @@ Instance* loadInstance(const char *fileName){
 	            for(j=0; j<n; j++)
 	            {
 	                a = fscanf(arq,"%d", &aux_2);
-			printf("%d ",aux_2);
+			
 	                inst->cost[iReturn(j,i,n,m)]=aux_2;
+			printf("%d ",inst->cost[iReturn(j,i,n,m)]);
 	            }
 	        }
 		getchar();
